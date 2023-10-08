a3749855dee834ee9f24127901057cb5f3142d0c
Ashantilogan <Ashantilogan2@gmail.com>
2023-10-08 15:55:26 -0400

    javascript functions continued

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`
+    } else {
+        return `This Saturday, I want to roller-skate!`
+    }
+}
+
+function mondayWork(activity) {
+    if (activity) {
+    return `This Monday, I will ${activity}.`
+} else {
+    return "This Monday, I will go to the office."
+}
+}
+function wrapAdjective(flair = "*"){
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`
+    }
+}
